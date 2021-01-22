4fdbaceeff715090f0e9e3ad2f01787c53fdebfc
Henrique Domanski <henrydomanski@gmail.com>
2021-01-22 17:29:46 -0300

    omnistack9

---
+++ aulas/backend/src/server.js
@@ -0,0 +1,29 @@
+const express = require ('express')
+const mongoose = require ('mongoose')
+const routes = require ('./routes')
+
+const app = express();
+
+
+mongoose.connect ('mongodb+srv://omnistack:omnistack@omnistack.urxuz.mongodb.net/semana09?retryWrites=true&w=majority',
+ {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+
+ })
+
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edição, delete)
+// req.body = Acessar corpo da requisição (para criação, edição)
+
+app.use (express.json())
+
+app.post ('/users', (req, res) => {
+    return res.json (req.body)
+
+});
+
+
+app.use (express.json());
+app.use (routes);
+app.listen(3333);
