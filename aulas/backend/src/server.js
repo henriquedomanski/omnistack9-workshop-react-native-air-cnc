e37435d974cfb022b8191d253c0b27985427d771
Henrique Domanski <henrydomanski@gmail.com>
2021-02-02 12:52:24 -0300

    update 2/2/21

--- aulas/backend/src/server.js
+++ aulas/backend/src/server.js
@@ -1,7 +1,10 @@
+
+
 const express = require ('express')
 const mongoose = require ('mongoose')
 const routes = require ('./routes')
 const cors = require ('cors')
+const path = require ('path')
 
 const app = express();
 
@@ -26,5 +29,8 @@ app.post ('/users', (req, res) => {
 
 app.use(cors())
 app.use (express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use (routes);
 app.listen(3333);
+
+
